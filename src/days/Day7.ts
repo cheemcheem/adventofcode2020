ad317e215b0b75dc00098fd208a75657dd443e8e
Kathan Cheema <kathancheema@outlook.com>
2020-12-17 19:07:21 +0000

    Day 7

---
+++ src/days/Day7.ts
@@ -0,0 +1,95 @@
+import {Day} from "../common";
+
+export default class Day7 extends Day {
+
+  private readonly bagMap = new Map<string, { colour: string, count: number }[]>();
+
+  constructor() {
+    super(7);
+  }
+
+  loadBagMap = async () => {
+    const splitString = this.getSplitString();
+
+    splitString.forEach(line => {
+      let [parentColour, childColours] = line.split(" bags contain ");
+      childColours = childColours.replaceAll(".", "");
+
+      if (childColours === "no other bags") {
+        this.bagMap.set(parentColour, []);
+
+      } else {
+        this.bagMap.set(
+            parentColour,
+            childColours.replaceAll(/( bag(s?))/g, "")
+            .split(", ")
+            .map(childColour => {
+              const count = Number(childColour.split(" ")[0]);
+              const colour = childColour.split(`${count} `)[1];
+              return {colour, count};
+            })
+        )
+
+      }
+    })
+  }
+
+  countBagsThatContainInputColour = (inputColour: string) => {
+    let count = 0;
+
+    this.bagMap.forEach((childBags) => {
+      const hasMatch = childBags
+      .map(({colour}) => {
+        if (colour === inputColour) return true;
+        return this.countBagsThatContainInputColourInColour(inputColour, colour);
+      })
+      .reduce((a, b) => a || b, false);
+
+      hasMatch && count++;
+    })
+
+    return count;
+  }
+
+  countBagsThatContainInputColourInColour = (inputColour: string, colour: string): boolean => {
+    return this.bagMap
+    .get(colour)!
+    .map(({colour: childColour}) => {
+      if (childColour === inputColour) return true;
+      return this.countBagsThatContainInputColourInColour(inputColour, childColour);
+    })
+    .reduce((a, b) => a || b, false);
+  }
+
+  part1 = async () => {
+    await this.loadBagMap();
+
+    const input = "shiny gold";
+    const answer = this.countBagsThatContainInputColour(input);
+
+    return {part: 1, answer};
+  }
+
+  countBagsInsideInputColour = (inputColour: string): number => {
+    const inputColourBags = this.bagMap.get(inputColour);
+
+    if (!inputColourBags) {
+      return 0
+    }
+
+    return inputColourBags
+    .map(({colour, count}) => count * (1 + this.countBagsInsideInputColour(colour)))
+    .reduce((a, b) => a + b, 0);
+
+  }
+
+  part2 = async () => {
+    await this.loadBagMap();
+
+    const input = "shiny gold";
+    const answer = this.countBagsInsideInputColour(input);
+
+    return {part: 2, answer};
+  }
+
+}
--- src/days/index.ts
+++ src/days/index.ts
@@ -4,5 +4,6 @@ import Day3 from "./Day3";
 import Day4 from "./Day4";
 import Day5 from "./Day5";
 import Day6 from "./Day6";
+import Day7 from "./Day7";
 
-export {Day1, Day2, Day3, Day4, Day5, Day6};
+export {Day1, Day2, Day3, Day4, Day5, Day6, Day7};
