a4448abfbf37802413ea28266c95f3bc720c6112
Kathan Cheema <kathancheema@outlook.com>
2020-12-18 02:22:48 +0000

    Add examples and change example usage.

--- src/index.ts
+++ src/index.ts
@@ -30,11 +30,9 @@ class Index {
             alias: 'l',
           },
           example: {
-            type: 'number',
             optional: true,
-            describe: 'Run example 1 or example 2 for the given day.',
-            alias: 'e',
-            choices: [1, 2],
+            describe: 'Run example input rather than real input for the given part/day.',
+            alias: 'e'
           }
         })
         .version(false)
@@ -51,22 +49,18 @@ class Index {
             throw new Error("Can't provide empty part option.")
           }
 
-          if (Object.keys(argv).includes("example") && !argv.example) {
-            throw new Error("Can't provide empty example.")
-          }
-
           return true;
 
         })
         .help()
         .usage('$0 run')
-        .usage('$0 run -e [example]')
+        .usage('$0 run -e')
         .usage('$0 run -d [day]')
-        .usage('$0 run -d [day] -e [example]')
+        .usage('$0 run -d [day] -e')
         .usage('$0 run -d [day] -p [part]')
-        .usage('$0 run -d [day] -p [part] -e [example]')
+        .usage('$0 run -d [day] -p [part] -e')
         .usage('$0 run -l')
-        .usage('$0 run -l -e [example]')
+        .usage('$0 run -l -e')
         .showHelpOnFail(true, "This can't be run with these options.")
         .parse()
     ;
@@ -74,57 +68,96 @@ class Index {
     if (!result) return;
 
     const part = result.part as 1 | 2 | undefined;
-    const example = result.example as 1 | 2 | undefined;
+    const hasExample = Object.keys(result).includes("example");
 
-    result.latest ? this.runOne(Index.DAYS.length, part, example)
-        : result.day ? this.runOne(result.day, part, example)
-        : this.runAll(example)
+    result.latest ? Index.runOne(Index.DAYS.length, part, hasExample)
+        : result.day ? Index.runOne(result.day, part, hasExample)
+        : Index.runAll(hasExample)
     ;
 
   }
 
-  async runAll(example?: 1 | 2) {
+  private static async getSolutions(example?: 1 | 2) {
     const puzzles = await Promise.all(
         Index.DAYS
         .map(async a => await new a() as Day)
         .map(async a => (await a).init(example))
     );
 
-    const solutions = await Promise.all(
+    if (example) {
+      return await Promise.all(
+          puzzles.flatMap(async a => [
+            {part: example, answer: await (example === 1 ? a.part1() : a.part2())}
+          ])
+      );
+    }
+
+    return await Promise.all(
         puzzles.flatMap(async a => [
           {part: 1, answer: await a.part1()},
           {part: 2, answer: await a.part2()}
         ])
     );
-
-    solutions.map(
-        async (solution, day) => {
-          day++;
-          console.log({
-            day,
-            solution
-          })
-        }
-    );
   }
 
-  async runOne(dayNumber: number, part?: 1 | 2, example?: 1 | 2) {
+  private static async runAll(hasExample?: boolean) {
+    let solutions;
+    if (hasExample) {
+      const solutions1 = await Index.getSolutions(1);
+      const solutions2 = await Index.getSolutions(2);
+
+      if (solutions1.length !== solutions2.length) throw ERROR_MESSAGE;
+
+      solutions = [];
+      for (let i = 0; i < solutions1.length; i++) {
+        solutions.push([...solutions1[i], ...solutions2[i]]);
+      }
+
+    } else {
+      solutions = await Index.getSolutions();
+    }
+
+    // console.log(JSON.stringify({solutions}, undefined, 4))
+
+    solutions.map((solution, day) => {
+      day++;
+      console.log({
+        day,
+        solution
+      });
+    });
+  }
+
+  private static async runOne(dayNumber: number, part?: 1 | 2, hasExample?: boolean) {
     if (dayNumber > Index.DAYS.length) {
       throw ERROR_MESSAGE;
     }
 
-    const day = await new Index.DAYS[dayNumber - 1]().init(example);
     const solution = [];
-    if (!part || part === 1) {
-      solution.push({part: 1, answer: await day.part1()})
+
+    if (hasExample) {
+      if (!part || part === 1) {
+        const day = await new Index.DAYS[dayNumber - 1]().init(1);
+        solution.push({part: 1, answer: await day.part1()})
+      }
+      if (!part || part === 2) {
+        const day = await new Index.DAYS[dayNumber - 1]().init(2);
+        solution.push({part: 2, answer: await day.part2()})
+      }
+    } else {
+      const day = await new Index.DAYS[dayNumber - 1]().init();
+      if (!part || part === 1) {
+        solution.push({part: 1, answer: await day.part1()});
+      }
+      if (!part || part === 2) {
+        solution.push({part: 2, answer: await day.part2()});
+      }
     }
-    if (!part || part === 2) {
-      solution.push({part: 2, answer: await day.part2()})
-    }
+
     console.log({
       day: dayNumber,
       solution
-    })
+    });
   }
 }
 
